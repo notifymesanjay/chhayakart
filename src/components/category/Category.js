1dc36b14f77c2e1b66bea2f9de06e68f58b521d4
sampath badri <sampath.badri99@gmail.com>
2023-07-13 22:57:10 +0530

    category section completed

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,39 +1,16 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
-import "./category.css";
-import api from "../../api/api";
-import { motion } from "framer-motion";
-import { BsPlusCircle, BsGrid3X3GapFill } from "react-icons/bs";
-import CategoryChild from "./CategoryChild";
-import { AiOutlineArrowRight, AiOutlineDown } from "react-icons/ai";
+import React, { useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { ActionTypes } from "../../model/action-type";
-import { Link, useNavigate } from "react-router-dom";
-import { Card } from "react-bootstrap";
-import Slider from "react-slick";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import SectionTitle from "../shared/section-title/section-title";
-import styles from "./category.module.scss";
 import ResponsiveCarousel from "../shared/responsive-carousel/responsive-carousel";
 import { useResponsive } from "../shared/use-responsive";
+import styles from "./category.module.scss";
 
 const Category = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isSmScreen } = useResponsive();
-
-  useEffect(() => {
-    console.log("xyz", isSmScreen);
-  }, [isSmScreen]);
-
-  const sliderRef = useRef(null);
-
-  const handlePrevClick = () => {
-    sliderRef.current.slickPrev();
-  };
-
-  const handleNextClick = () => {
-    sliderRef.current.slickNext();
-  };
 
   const shop = useSelector((state) => state.shop);
 
@@ -41,66 +18,26 @@ const Category = () => {
     dispatch({ type: ActionTypes.SET_FILTER_CATEGORY, payload: category.id });
     navigate("/products");
   };
-  const settings = {
-    infinite: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: false,
-    direction: "rtl",
-    pauseOnDotsHover: false,
-    pauseOnFocus: true,
-    slidesToShow: 5,
-    slidesPerRow: 1,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 4,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-        },
-      },
-
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 425,
-        settings: {
-          slidesToShow: 3,
-          rows: 2,
-        },
-      },
-    ],
-  };
-
-  useEffect(() => {
-    console.log("xyz", shop.shop.category);
-  }, [shop]);
 
   return useMemo(
     () => (
       <>
-        {shop.shop === null ? (
-          <></>
-        ) : (
+        {shop.shop !== null && (
           <>
             <div className={styles.categoryWrapper}>
-              <SectionTitle title={"Shop By Category"} linkText={false} />
+              <SectionTitle
+                title={"Shop By Category"}
+                linkText={true}
+                linkName={!isSmScreen ? "See All Categories" : "See All"}
+                onLinkClick={() => {
+                  navigate("/categories");
+                }}
+              />
             </div>
-            {!isSmScreen && (
+            {!isSmScreen ? (
               <ResponsiveCarousel
                 items={5}
-                itemsInTablet={1}
+                itemsInTablet={3}
                 infinite={true}
                 autoPlay={true}
                 autoPlaySpeed={4000}
@@ -123,12 +60,27 @@ const Category = () => {
                   </div>
                 ))}
               </ResponsiveCarousel>
+            ) : (
+              <div className={styles.mobCategoryWrapper}>
+                {shop.shop.category.map((ctg, index) => (
+                  <div className={styles.category} key={index}>
+                    <div className={styles.imageWrapper}>
+                      <img
+                        className={styles.categoryImg}
+                        src={ctg.image_url}
+                        alt={ctg.subtitle}
+                      />
+                    </div>
+                    <p className={styles.categoryName}>{ctg.name}</p>
+                  </div>
+                ))}
+              </div>
             )}
           </>
         )}
       </>
     ),
-    [shop]
+    [shop, isSmScreen, navigate]
   );
 };
 
--- src/components/shared/section-title/section-title.js
+++ src/components/shared/section-title/section-title.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./section-title.module.scss";
 
-const SectionTitle = ({ title = "", linkText = true, className = "" }) => {
+const SectionTitle = ({
+  title = "",
+  linkText = false,
+  className = "",
+  linkName = "",
+  onLinkClick=() => {},
+}) => {
   return (
     <div className={`${styles.heading} ${className}`}>
-      {linkText && <p>Hello</p>}
       <div className={styles.headingWrapper}>
         <h2 className={styles.header}>{title}</h2>
       </div>
+
+      {linkText && <p className={styles.link} onClick={onLinkClick}>{linkName}</p>}
     </div>
   );
 };
