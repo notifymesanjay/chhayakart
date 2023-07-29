274ed76a16df9a8c6c5909f8cfb5a97b06f2be6b
akshay-rajputh <notifyme.akshay@gmail.com>
2023-07-29 17:57:26 +0530

    cha

--- src/components/header/sidebar.js
+++ src/components/header/sidebar.js
@@ -19,7 +19,8 @@ const Sidebar = ({ setIsLogin = () => {}, closeRef }) => {
 	const dispatch = useDispatch();
 	const cookies = new Cookies();
 
-<<<<<<< HEAD
+	const [isSubMenu, setIsSubMenu] = useState(false);
+
 	const handleLogout = () => {
 		api
 			.logout(cookies.get("jwt_token"))
@@ -36,26 +37,6 @@ const Sidebar = ({ setIsLogin = () => {}, closeRef }) => {
 				}
 			});
 	};
-=======
-  const [isSubMenu, setIsSubMenu] = useState(false);
-
-  const handleLogout = () => {
-    api
-      .logout(cookies.get("jwt_token"))
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.status === 1) {
-          cookies.remove("jwt_token");
-          removelocalstorageOTP();
-          dispatch({ type: ActionTypes.LOGOUT_AUTH, payload: null });
-          toast.success("You're Successfully Logged Out");
-          navigate("/");
-        } else {
-          toast.info(result.message);
-        }
-      });
-  };
->>>>>>> main
 
 	const handleWishlist = () => {
 		if (user.status === "loading") {
@@ -73,7 +54,6 @@ const Sidebar = ({ setIsLogin = () => {}, closeRef }) => {
 		}
 	};
 
-<<<<<<< HEAD
 	return (
 		<div>
 			<div className={styles.headerWrapper}>
@@ -85,7 +65,7 @@ const Sidebar = ({ setIsLogin = () => {}, closeRef }) => {
 						aria-label="Close"
 						ref={closeRef}
 					/>
-					<p className={styles.header}>Hello! </p>
+					<p className={styles.header}> Hello! </p>
 				</div>
 				{user.status === "loading" && (
 					<button
@@ -151,51 +131,66 @@ const Sidebar = ({ setIsLogin = () => {}, closeRef }) => {
 				<li className={`${styles.listItem} ${styles.disableItem}`}>
 					Donate<span className={styles.comingSoon}>( Coming Soon )</span>
 				</li>
-				<p className={styles.subHeader}>Help & Support</p>
-				<li
-					className={styles.listItem}
-					data-bs-dismiss="offcanvas"
-					aria-label="Close"
-					ref={closeRef}
+				<p
+					className={styles.subMenuHeader}
 					onClick={() => {
-						navigate("/about");
+						setIsSubMenu(!isSubMenu);
 					}}
 				>
-					About Chhayakart
-				</li>
-				<li
-					className={styles.listItem}
-					data-bs-dismiss="offcanvas"
-					aria-label="Close"
-					ref={closeRef}
-					onClick={() => {
-						navigate("/terms");
-					}}
-				>
-					Chhayakart Terms
-				</li>
-				<li
-					className={styles.listItem}
-					data-bs-dismiss="offcanvas"
-					aria-label="Close"
-					ref={closeRef}
-					onClick={() => {
-						navigate("/policy/Privacy_Policy");
-					}}
-				>
-					Chhayakart Policies
-				</li>
-				<li
-					className={styles.listItem}
-					data-bs-dismiss="offcanvas"
-					aria-label="Close"
-					ref={closeRef}
-					onClick={() => {
-						navigate("/contact");
-					}}
-				>
-					Contact Us
-				</li>
+					About Chhayakart{" "}
+					<FontAwesomeIcon
+						className={isSubMenu ? styles.activeIcon : ""}
+						icon={faAngleDown}
+					/>
+				</p>
+				{isSubMenu && (
+					<ul className={`${styles.listWrapper} ${styles.subWrapper}`}>
+						<li
+							className={styles.listItem}
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"
+							ref={closeRef}
+							onClick={() => {
+								navigate("/about");
+							}}
+						>
+							About Us
+						</li>
+						<li
+							className={styles.listItem}
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"
+							ref={closeRef}
+							onClick={() => {
+								navigate("/terms");
+							}}
+						>
+							Chhayakart Terms
+						</li>
+						<li
+							className={styles.listItem}
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"
+							ref={closeRef}
+							onClick={() => {
+								navigate("/policy/Privacy_Policy");
+							}}
+						>
+							Chhayakart Policies
+						</li>
+						<li
+							className={styles.listItem}
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"
+							ref={closeRef}
+							onClick={() => {
+								navigate("/contact");
+							}}
+						>
+							Contact Us
+						</li>
+					</ul>
+				)}
 				{user.status !== "loading" && (
 					<li
 						className={styles.listItem}
@@ -223,172 +218,6 @@ const Sidebar = ({ setIsLogin = () => {}, closeRef }) => {
 			</div>
 		</div>
 	);
-=======
-  return (
-    <div>
-      <div className={styles.headerWrapper}>
-        <div className={styles.iconWrapper}>
-          <FontAwesomeIcon
-            icon={faAngleLeft}
-            className={styles.backIcon}
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-            ref={closeRef}
-          />
-          <p className={styles.header}> Gaon Ki Dukaan Me Swagat Hai </p>
-        </div>
-        {user.status === "loading" && (
-          <button
-            className={styles.loginBtn}
-            onClick={() => {
-              setIsLogin(true);
-            }}
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-            ref={closeRef}
-          >
-            Login
-          </button>
-        )}
-      </div>
-      <ul className={styles.listWrapper}>
-        <li
-          className={styles.listItem}
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-          ref={closeRef}
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Home
-        </li>
-        <li
-          className={styles.listItem}
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-          ref={closeRef}
-          onClick={() => {
-            navigate("/subCategory/96");
-          }}
-        >
-          Season Special
-        </li>
-        <li
-          className={styles.listItem}
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-          ref={closeRef}
-          onClick={handleOrders}
-        >
-          My Orders
-        </li>
-        <li
-          className={styles.listItem}
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-          ref={closeRef}
-          onClick={handleWishlist}
-        >
-          My Wishlist
-        </li>
-        <li className={`${styles.listItem} ${styles.disableItem}`}>
-          Chhaya Purse<span className={styles.comingSoon}>( Coming Soon )</span>
-        </li>
-        <li className={`${styles.listItem} ${styles.disableItem}`}>
-          Share & Earn<span className={styles.comingSoon}>( Coming Soon )</span>
-        </li>
-        <li className={`${styles.listItem} ${styles.disableItem}`}>
-          Donate<span className={styles.comingSoon}>( Coming Soon )</span>
-        </li>
-        <p
-          className={styles.subMenuHeader}
-          onClick={() => {
-            setIsSubMenu(!isSubMenu);
-          }}
-        >
-          About Chhayakart{" "}
-          <FontAwesomeIcon
-            className={isSubMenu ? styles.activeIcon : ""}
-            icon={faAngleDown}
-          />
-        </p>
-        {isSubMenu && (
-          <ul className={`${styles.listWrapper} ${styles.subWrapper}`}>
-            <li
-              className={styles.listItem}
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-              ref={closeRef}
-              onClick={() => {
-                navigate("/about");
-              }}
-            >
-              About Us
-            </li>
-            <li
-              className={styles.listItem}
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-              ref={closeRef}
-              onClick={() => {
-                navigate("/terms");
-              }}
-            >
-              Chhayakart Terms
-            </li>
-            <li
-              className={styles.listItem}
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-              ref={closeRef}
-              onClick={() => {
-                navigate("/policy/Privacy_Policy");
-              }}
-            >
-              Chhayakart Policies
-            </li>
-            <li
-              className={styles.listItem}
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-              ref={closeRef}
-              onClick={() => {
-                navigate("/contact");
-              }}
-            >
-              Contact Us
-            </li>
-          </ul>
-        )}
-        {user.status !== "loading" && (
-          <li
-            className={styles.listItem}
-            onClick={handleLogout}
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-            ref={closeRef}
-          >
-            Logout
-          </li>
-        )}
-      </ul>
-      <div className={styles.bottomSection}>
-        <div className={styles.downloadAppWrapper}>
-          <p className={styles.downloadAppHeader}>Download the App</p>
-          <a href="https://play.google.com/store/apps/details?id=com.chayakart">
-            <img src={DownloadApp} alt="app-download" />
-          </a>
-        </div>
-        <img
-          className={styles.logo}
-          src={ChhayakartPinkLogo}
-          alt="chhayakart-logo"
-        />
-      </div>
-    </div>
-  );
->>>>>>> main
 };
 
 export default Sidebar;
