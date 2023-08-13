dc9214c62166f9650a623f82ed2eaf81864cdc11
akshay-rajputh <notifyme.akshay@gmail.com>
2023-08-13 13:42:59 +0530

    slice

--- src/components/checkout/Success.js
+++ src/components/checkout/Success.js
@@ -3,18 +3,20 @@ import Order from "../order/Order";
 import styles from "./Success.module.scss";
 //import orderplaced from "../orderplased.avif";
 import thanks from "../thanks.webp";
+import ynks from "../ynks.webp";
+
 export default function Success() {
 	return (
 		<>
 			<div className={styles.cover}>
 				<img
-					data-src={thanks}
+					data-src={ynks}
 					className={`${styles.img} lazyload`}
 					alt="order placed thankyou visit again"
 				></img>
 				{/* <span>THANKS.... </span> */}
 				<div className={styles.title}>
-					<h1 className="title">YOUR ORDER HISTORY WITH CHHAYAKART</h1>
+					<h1 className="title">YOUR ORDER DETAILS WITH CHHAYAKART</h1>
 				</div>
 			</div>
 			<Order />
