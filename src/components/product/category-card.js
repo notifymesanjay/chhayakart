59363ff08816c4fb94cde381dd534bfc5e684ff3
akshay-rajputh <notifyme.akshay@gmail.com>
2023-08-30 20:34:39 +0530

    newBanners

--- src/components/product/category-card.js
+++ src/components/product/category-card.js
@@ -4,310 +4,331 @@ import { useHref, useNavigate } from "react-router-dom";
 import { useResponsive } from "../shared/use-responsive";
 import SBI from "../SBI.webp";
 import AU from "../AU.webp";
-
+import ShingadaLaddo from "../ShingadaLaddo.jpg";
+import BatataSabudana from "../BatataSabudana.jpg";
+import KhajurDryfruit from "../KhajurDryfruit.jpg";
+import SabudanaPremix from "../SabudanaPremix.jpg";
 import INDUSIND from "../INDUSIND.webp";
+import allOffers from "../allOffers.jpg";
+import order4999 from "../order4999.jpg";
+import order9999 from "../order9999.jpg";
+import freeUpwasKit from "../freeUpwasKit.jpg";
+import shirdiladdu from "../shirdiladdu.jpg";
+import mahakalU from "../mahakalU.jpg";
+import maharashtrianChakli from "../maharashtrianChakli.jpg";
+import khurdai from "../khurdai.jpg";
+import vade from "../vade.jpg";
+import ragiPapad from "../ragiPapad.jpg";
+import crunchyPatra from "../crunchyPatra .jpg";
+import chatpataGahu from "../chatpataGahu.jpg";
 import ResponsiveCarousel from "../shared/responsive-carousel/responsive-carousel";
 const CategoryCard = ({ subCategories = [], setSelectedFilter = () => {} }) => {
-  const navigate = useNavigate();
-  const { isSmScreen } = useResponsive();
-  const dynamicBanners = [
-    [
-      {
-        id: 1,
-        image: SBI,
-        title: "banner1",
-        link: "subCategory/96",
-      },
-      {
-        id: 2,
-        image: AU,
-        title: "banner2 ",
-        link: "subCategory/96",
-      },
-      {
-        id: 3,
-        image: INDUSIND,
-        title: "banner3",
-        link: "subCategory/96",
-      },
-    ],
-    [
-      {
-        id: 1,
-        image: SBI,
-        title: "banner1",
-        link: "subCategory/96",
-      },
-      {
-        id: 2,
-        image: AU,
-        title: "banner2 ",
-        link: "subCategory/96",
-      },
-      {
-        id: 3,
-        image: INDUSIND,
-        title: "banner3",
-        link: "subCategory/96",
-      },
-    ],
-    [
-      {
-        id: 1,
-        image: SBI,
-        title: "banner1",
-        link: "subCategory/96",
-      },
-      {
-        id: 2,
-        image: AU,
-        title: "banner2 ",
-        link: "subCategory/96",
-      },
-      {
-        id: 3,
-        image: INDUSIND,
-        title: "banner3",
-        link: "subCategory/96",
-      },
-    ],
-    [
-      {
-        id: 1,
-        image: SBI,
-        title: "banner1",
-        link: "subCategory/96",
-      },
-      {
-        id: 2,
-        image: AU,
-        title: "banner2 ",
-        link: "subCategory/96",
-      },
-      {
-        id: 3,
-        image: INDUSIND,
-        title: "banner3",
-        link: "subCategory/96",
-      },
-    ],
-    [
-      {
-        id: 1,
-        image: SBI,
-        title: "banner1",
-        link: "subCategory/96",
-      },
-      {
-        id: 2,
-        image: AU,
-        title: "banner2 ",
-        link: "subCategory/96",
-      },
-      {
-        id: 3,
-        image: INDUSIND,
-        title: "banner3",
-        link: "subCategory/96",
-      },
-    ],
-    [
-      {
-        id: 1,
-        image: SBI,
-        title: "banner1",
-        link: "subCategory/96",
-      },
-      {
-        id: 2,
-        image: AU,
-        title: "banner2 ",
-        link: "subCategory/96",
-      },
-      {
-        id: 3,
-        image: INDUSIND,
-        title: "banner3",
-        link: "subCategory/96",
-      },
-    ],
-  ];
-  const banners = [
-    {
-      id: 1,
-      image: SBI,
-      title: "banner1",
-      link: "https://api.earnow.in/l/c4HDPrmGOi",
-    },
-    {
-      id: 2,
-      image: AU,
-      title: "banner2 ",
-      link: "https://api.earnow.in/l/Q6hkysjezl",
-    },
-    {
-      id: 3,
-      image: INDUSIND,
-      title: "banner3",
-      link: "https://api.earnow.in/l/PkFqRHbgH9",
-    },
-  ];
-  return isSmScreen ? (
-    <>
-      {subCategories.slice(0, 12).map((subCategory, index) => (
-        <div key={index}>
-          {index % 2 === 0 && (
-            <div
-              className={`${styles.BankBanner} ${
-                index === 0 && styles.hideBanner
-              }`}
-            >
-              <ResponsiveCarousel
-                items={1}
-                itemsInTablet={1}
-                itemsInMobile={1}
-                infinite={true}
-                autoPlaySpeed={3500}
-                showArrows={false}
-                showDots={true}
-                autoPlay={true}
-                partialVisibilityGutter={false}
-              >
-                {dynamicBanners[index / 2].map((img) => (
-                  <div key={img.id}>
-                    <img
-                      onClick={() => {
-                        navigate(img.link);
-                      }}
-                      className={styles.banner}
-                      src={img.image}
-                      alt={img.title}
-                    />
-                  </div>
-                ))}
-              </ResponsiveCarousel>
-            </div>
-          )}
-          <div className={styles.homeCategoryWrapper} key={index}>
-            <div className={styles.headerWrapper}>
-              <h1 className={styles.header}>{subCategory.category_name}</h1>
-            </div>
-            <div className={styles.subCategoryWrapper}>
-              {subCategory.sub_category.map((sub_ctg, index1) => (
-                <div
-                  className={styles.subCategoryCard}
-                  key={index1}
-                  onClick={() => {
-                    navigate(
-                      `/subCategory/${subCategory.category_id}/${sub_ctg.id}_${sub_ctg.title}`
-                    );
-                    setSelectedFilter(sub_ctg.id);
-                  }}
-                >
-                  <div className={styles.imageWrapper}>
-                    <img
-                      className={styles.subCategoryImg}
-                      src={sub_ctg.image_url}
-                      alt="Catogery"
-                    />
-                  </div>
-                  <div className={styles.cardBody}>
-                    <p className={styles.title}>{sub_ctg.title}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      ))}
-      <div className={styles.BankBanner}>
-        <ResponsiveCarousel
-          items={1}
-          itemsInTablet={1}
-          itemsInMobile={1}
-          infinite={true}
-          autoPlaySpeed={3500}
-          showArrows={false}
-          showDots={true}
-          autoPlay={true}
-          partialVisibilityGutter={false}
-        >
-          {banners.map((img) => (
-            <div key={img.id}>
-              <a href={img.link}>
-                <img
-                  className={styles.banner}
-                  src={img.image}
-                  alt={img.title}
-                />
-              </a>
-            </div>
-          ))}
-        </ResponsiveCarousel>
-      </div>
-    </>
-  ) : (
-    <div className="container">
-      {subCategories.slice(0, 12).map((subCategory) => (
-        <div className={styles.cardWrapper}>
-          <div className={styles.headerWrapper}>
-            <h1 className={styles.header}>{subCategory.category_name}</h1>
-          </div>
-          <div className={styles.categoryWrapper}>
-            <div
-              className={styles.categoryImgWrapper}
-              onClick={() => {
-                navigate(`/subCategory/${subCategory.category_id}`);
-                setSelectedFilter(0);
-              }}
-            >
-              <img
-                src={`${
-                  subCategory.category_image.split(".webp")[0]
-                }_desktop.webp`}
-                alt="category-img"
-              />
+	const navigate = useNavigate();
+	const { isSmScreen } = useResponsive();
+	const dynamicBanners = [
+		[
+			{
+				id: 1,
+				image: SBI,
+				title: "banner1",
+				link: "subCategory/96",
+			},
+			{
+				id: 2,
+				image: AU,
+				title: "banner2 ",
+				link: "subCategory/96",
+			},
+			{
+				id: 3,
+				image: INDUSIND,
+				title: "banner3",
+				link: "subCategory/96",
+			},
+		],
+		[
+			{
+				id: 1,
+				image: ShingadaLaddo,
+				title: "banner1",
+				link: "subCategory/96",
+			},
+			{
+				id: 2,
+				image: BatataSabudana,
+				title: "banner2 ",
+				link: "subCategory/96",
+			},
+			{
+				id: 3,
+				image: SabudanaPremix,
+				title: "banner3",
+				link: "subCategory/96",
+			},
+			{
+				id: 4,
+				image: KhajurDryfruit,
+				title: "banner4",
+				link: "subCategory/96",
+			},
+		],
+		[
+			{
+				id: 1,
+				image: allOffers,
+				title: "banner1",
+				link: "/",
+			},
+			{
+				id: 2,
+				image: freeUpwasKit,
+				title: "banner2 ",
+				link: "/",
+			},
+			{
+				id: 3,
+				image: order4999,
+				title: "banner3",
+				link: "/",
+			},
+			{
+				id: 4,
+				image: order9999,
+				title: "banner4",
+				link: "subCategory/96",
+			},
+		],
+		[
+			{
+				id: 1,
+				image: shirdiladdu,
+				title: "shirdiladdu",
+				link: "subCategory/98/29_PRASAD",
+			},
+			{
+				id: 2,
+				image: mahakalU,
+				title: "mahakalU ",
+				link: "subCategory/98/29_PRASAD",
+			},
+		],
+		[
+			{
+				id: 1,
+				image: crunchyPatra,
+				title: "crunchyPatra",
+				link: "subCategory/100/41_HOMEMADE%20DESI%20SNACK",
+			},
+			{
+				id: 2,
+				image: maharashtrianChakli,
+				title: "maharashtrianChakli ",
+				link: "subCategory/100/41_HOMEMADE%20DESI%20SNACK",
+			},
+			{
+				id: 3,
+				image: chatpataGahu,
+				title: "chatpataGahu",
+				link: "subCategory/100/41_HOMEMADE%20DESI%20SNACK",
+			},
+		],
+		[
+			{
+				id: 1,
+				image: ragiPapad,
+				title: "ragiPapad",
+				link: "subCategory/94/31_PAPAD",
+			},
+			{
+				id: 2,
+				image: khurdai,
+				title: "khurdai ",
+				link: "subCategory/94/32_KURDAI",
+			},
+			{
+				id: 3,
+				image: vade,
+				title: "vade",
+				link: "subCategory/94/82_VADE",
+			},
+		],
+	];
+	const banners = [
+		{
+			id: 1,
+			image: SBI,
+			title: "banner1",
+			link: "https://api.earnow.in/l/c4HDPrmGOi",
+		},
+		{
+			id: 2,
+			image: AU,
+			title: "banner2 ",
+			link: "https://api.earnow.in/l/Q6hkysjezl",
+		},
+		{
+			id: 3,
+			image: INDUSIND,
+			title: "banner3",
+			link: "https://api.earnow.in/l/PkFqRHbgH9",
+		},
+	];
+	return isSmScreen ? (
+		<>
+			{subCategories.slice(0, 12).map((subCategory, index) => (
+				<div key={index}>
+					{index % 2 === 0 && (
+						<div
+							className={`${styles.BankBanner} ${
+								index === 0 && styles.hideBanner
+							}`}
+						>
+							<ResponsiveCarousel
+								items={1}
+								itemsInTablet={1}
+								itemsInMobile={1}
+								infinite={true}
+								autoPlaySpeed={3500}
+								showArrows={false}
+								showDots={true}
+								autoPlay={true}
+								partialVisibilityGutter={false}
+							>
+								{dynamicBanners[index / 2].map((img) => (
+									<div key={img.id}>
+										<img
+											onClick={() => {
+												navigate(img.link);
+											}}
+											className={styles.banner}
+											src={img.image}
+											alt={img.title}
+										/>
+									</div>
+								))}
+							</ResponsiveCarousel>
+						</div>
+					)}
+					<div className={styles.homeCategoryWrapper} key={index}>
+						<div className={styles.headerWrapper}>
+							<h1 className={styles.header}>{subCategory.category_name}</h1>
+						</div>
+						<div className={styles.subCategoryWrapper}>
+							{subCategory.sub_category.map((sub_ctg, index1) => (
+								<div
+									className={styles.subCategoryCard}
+									key={index1}
+									onClick={() => {
+										navigate(
+											`/subCategory/${subCategory.category_id}/${sub_ctg.id}_${sub_ctg.title}`
+										);
+										setSelectedFilter(sub_ctg.id);
+									}}
+								>
+									<div className={styles.imageWrapper}>
+										<img
+											className={styles.subCategoryImg}
+											src={sub_ctg.image_url}
+											alt="Catogery"
+										/>
+									</div>
+									<div className={styles.cardBody}>
+										<p className={styles.title}>{sub_ctg.title}</p>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			))}
+			<div className={styles.BankBanner}>
+				<ResponsiveCarousel
+					items={1}
+					itemsInTablet={1}
+					itemsInMobile={1}
+					infinite={true}
+					autoPlaySpeed={3500}
+					showArrows={false}
+					showDots={true}
+					autoPlay={true}
+					partialVisibilityGutter={false}
+				>
+					{banners.map((img) => (
+						<div key={img.id}>
+							<a href={img.link}>
+								<img
+									className={styles.banner}
+									src={img.image}
+									alt={img.title}
+								/>
+							</a>
+						</div>
+					))}
+				</ResponsiveCarousel>
+			</div>
+		</>
+	) : (
+		<div className="container">
+			{subCategories.slice(0, 12).map((subCategory) => (
+				<div className={styles.cardWrapper}>
+					<div className={styles.headerWrapper}>
+						<h1 className={styles.header}>{subCategory.category_name}</h1>
+					</div>
+					<div className={styles.categoryWrapper}>
+						<div
+							className={styles.categoryImgWrapper}
+							onClick={() => {
+								navigate(`/subCategory/${subCategory.category_id}`);
+								setSelectedFilter(0);
+							}}
+						>
+							<img
+								src={`${
+									subCategory.category_image.split(".webp")[0]
+								}_desktop.webp`}
+								alt="category-img"
+							/>
 
-              {/* <div className={styles.viewAll}>
+							{/* <div className={styles.viewAll}>
 								{" "}
 								<button className={styles.view}> APPLY NOW</button>{" "}
 							</div> */}
-            </div>
-            <div className={styles.bodyWrapper}>
-              <div className={styles.subCategoryWrapper}>
-                {subCategory.sub_category.map((sub_ctg, index1) => (
-                  <div
-                    className={styles.subCategoryCard}
-                    key={index1}
-                    onClick={() => {
-                      navigate(
-                        `/subCategory/${subCategory.category_id}/${sub_ctg.id}_${sub_ctg.title}`
-                      );
-                      setSelectedFilter(sub_ctg.id);
-                    }}
-                  >
-                    <div className={styles.imageWrapper}>
-                      <img
-                        className={styles.subCategoryImg}
-                        src={`${
-                          sub_ctg.image_url &&
-                          sub_ctg.image_url.split(".webp")[0]
-                        }_desktop.webp`}
-                        alt="Catogery"
-                      />
-                    </div>
-                    {/* <div className={styles.cardBody}>
+						</div>
+						<div className={styles.bodyWrapper}>
+							<div className={styles.subCategoryWrapper}>
+								{subCategory.sub_category.map((sub_ctg, index1) => (
+									<div
+										className={styles.subCategoryCard}
+										key={index1}
+										onClick={() => {
+											navigate(
+												`/subCategory/${subCategory.category_id}/${sub_ctg.id}_${sub_ctg.title}`
+											);
+											setSelectedFilter(sub_ctg.id);
+										}}
+									>
+										<div className={styles.imageWrapper}>
+											<img
+												className={styles.subCategoryImg}
+												src={`${
+													sub_ctg.image_url &&
+													sub_ctg.image_url.split(".webp")[0]
+												}_desktop.webp`}
+												alt="Catogery"
+											/>
+										</div>
+										{/* <div className={styles.cardBody}>
 											<p className={styles.title}>{sub_ctg.title}</p>
 										</div> */}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default CategoryCard;
