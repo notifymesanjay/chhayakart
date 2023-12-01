cb6706180459d36f730e2d3f6557733c79d77b3d
gaurav <imgauravchandani@gmail.com>
2023-12-01 11:55:42 +0530

    changes: font size fix in tracking dialog

--- src/components/order/TrackingModalContent.jsx
+++ src/components/order/TrackingModalContent.jsx
@@ -48,7 +48,7 @@ const TrackingModalContent = ({trackingDetails, trackingPercentStatus, cancelled
                         }
                         <span class="step-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg></span>
                         Shipped
-                        {trackingDetails != null ? <div className="small text-black-50">
+                        {trackingDetails != null ? <div className="small text-black-50" style={{fontSize: '13px'}}>
                             {(trackingPercentStatus < 25) ? <></> : function (){
                                 if(trackingPercentStatus <= 50){
                                     let shipedActivity = trackingDetails.shipment_track_activities[0];
